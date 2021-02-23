3d2c572cbb02252b2ffc277e766b59f2bdb17922
rachmadcp <c14180083@john.petra.ac.id>
2021-02-23 18:25:32 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  hasil = 0;
+  a=0;
+  b=0;
+
+
+item = [
+  {nama : "Jeruk", harga : 15000, satuan: "kg"},
+  {nama : "Apel", harga : 5000, satuan: "kg"},
+  {nama : "Pisang", harga : 8000, satuan: "kg"},
+  {nama : "Durian", harga : 12000, satuan: "kg"}
+]
+
+tampil : boolean = true;
+tulisanopen : String;
+
+
+OPEN() {
+  this.tulisanopen="TOKOPEDIA";
+  alert("dibuka!")
+}
 }
